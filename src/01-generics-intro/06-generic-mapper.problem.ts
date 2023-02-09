6b727b71e089938ce98996fbe7a6aebaa8306bab
Bjørnar Hvidsten <devenania@proton.me>
2023-02-09 23:56:22 +0100

    exercise 06 passed

--- src/01-generics-intro/06-generic-mapper.problem.ts
+++ src/01-generics-intro/06-generic-mapper.problem.ts
@@ -1,7 +1,12 @@
 import { expect, it } from 'vitest';
 import { Equal, Expect } from '../helpers/type-utils';
 
-export const concatenateFirstNameAndLastName = (user: unknown) => {
+type User = {
+  firstName: string;
+  lastName: string;
+};
+
+export const concatenateFirstNameAndLastName = <T extends User>(user: T) => {
   return {
     ...user,
     fullName: `${user.firstName} ${user.lastName}`,
