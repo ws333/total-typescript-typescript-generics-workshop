3360fbda30670ef34711158daee1a01676ecf49d
Bjørnar Hvidsten <devenania@proton.me>
2023-02-09 23:40:58 +0100

    exercise 03 passed

--- src/01-generics-intro/03-multiple-generics.problem.ts
+++ src/01-generics-intro/03-multiple-generics.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from 'vitest';
 import { Equal, Expect } from '../helpers/type-utils';
 
-const returnBothOfWhatIPassIn = (a: unknown, b: unknown) => {
+const returnBothOfWhatIPassIn = <T, S>(a: T, b: S) => {
   return {
     a,
     b,
